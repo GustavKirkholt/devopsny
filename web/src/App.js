bb1b042a40ee081203c8e3d9a7c47b259eeb1b1b
gustav <kirkholtgustav@gmail.com>
2021-10-13 17:40:55 +0200

    Tilfoejet knap

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {Button} from "@material-ui/core";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <h1>Test React Build!</h1>
+          <Button>KNAP</Button>
       </header>
     </div>
   );
